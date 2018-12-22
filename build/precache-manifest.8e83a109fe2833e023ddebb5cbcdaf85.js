8de1f8b264f47d28f2086b806a8d57a6d8b02162
Gabriel <gabriel@Gabriels-MacBook-Pro.local>
2018-12-22 17:30:27 -0500

    prod

---
+++ build/precache-manifest.8e83a109fe2833e023ddebb5cbcdaf85.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "79a303c840f7b5b039ef",
+    "url": "/stat-compare/static/css/main.359bb9e1.chunk.css"
+  },
+  {
+    "revision": "79a303c840f7b5b039ef",
+    "url": "/stat-compare/static/js/main.79a303c8.chunk.js"
+  },
+  {
+    "revision": "e055be3da3306f126584",
+    "url": "/stat-compare/static/css/1.131f2289.chunk.css"
+  },
+  {
+    "revision": "e055be3da3306f126584",
+    "url": "/stat-compare/static/js/1.e055be3d.chunk.js"
+  },
+  {
+    "revision": "bc0d2694c41f5f583fc4",
+    "url": "/stat-compare/static/css/2.9d95220c.chunk.css"
+  },
+  {
+    "revision": "bc0d2694c41f5f583fc4",
+    "url": "/stat-compare/static/js/2.bc0d2694.chunk.js"
+  },
+  {
+    "revision": "80fad6f271ae59cb92b7",
+    "url": "/stat-compare/static/css/3.7c163458.chunk.css"
+  },
+  {
+    "revision": "80fad6f271ae59cb92b7",
+    "url": "/stat-compare/static/js/3.80fad6f2.chunk.js"
+  },
+  {
+    "revision": "7afcf4cfb01a7415bf11",
+    "url": "/stat-compare/static/js/4.7afcf4cf.chunk.js"
+  },
+  {
+    "revision": "a07e75e7c713385ba3fa",
+    "url": "/stat-compare/static/js/runtime~main.a07e75e7.js"
+  },
+  {
+    "revision": "d34cd8af5a2641b5d901597b200465c8",
+    "url": "/stat-compare/static/media/avatar.d34cd8af.png"
+  },
+  {
+    "revision": "8ee64ca57d69dc3f1d78f8d9e4f6e06f",
+    "url": "/stat-compare/index.html"
+  }
+];
